8347a64aaf5fe742fafe7dea6f0801d0155852f1
bibek shakya <bibekshakya35@gmail.com>
2018-10-11 14:09:06 +0545

    MUSIC STORE & VOTING APP

---
+++ VotingApp/VotingApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using VotingApp.Models;
+namespace VotingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+            if(oldColor != null)
+            {
+                Votes.ChangeVote(color, (Color)oldColor);
+            }
+            else
+            {
+                Votes.RecordVote(color);
+            }
+            ViewBag.SelectedColor = Session["Color"] = color;
+            return View();
+        }
+    }
+}
